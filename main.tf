e414bd6ddf1a9189a721083bb6e68215d76b5aca
raviteja <ravitejatfcdemo@gmail.com>
2023-02-25 12:03:41 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -31,18 +31,24 @@ resource "aws_security_group" "elasticache" {
   )
 }
 
-resource "aws_elasticache_replication_group" "elasticache" {
-  replication_group_id       = "${var.env}-elasticache"
-  description                = "${var.env}-elasticache"
-  node_type                  = var.node_type
-  port                       = 6379
-  automatic_failover_enabled = true
-  subnet_group_name = aws_elasticache_subnet_group.default.name
+resource "aws_elasticache_cluster" "elasticache" {
+  cluster_id         = "${var.env}-elasticache-cluster"
+  engine             = "redis"
+  node_type          = var.node_type
+  num_cache_nodes    = var.num_cache_nodes
+  engine_version     = var.engine_version
+  port               = 6379
+  subnet_group_name  = aws_elasticache_subnet_group.default.name
   security_group_ids = [aws_security_group.elasticache.id]
-  num_node_groups = var.num_node_groups
-  replicas_per_node_group = var.replicas_per_node_group
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-elasticache" }
   )
 }
+
+resource "aws_ssm_parameter" "elasticache_endpoint" {
+  name  = "${var.env}.elasticache.ENDPOINT"
+  type  = "String"
+  value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
+}
+
--- vars.tf
+++ vars.tf
@@ -2,6 +2,6 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
-variable "replicas_per_node_group" {}
-variable "num_node_groups" {}
+variable "num_cache_nodes" {}
 variable "node_type" {}
+variable "engine_version" {}
